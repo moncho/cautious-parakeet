c63b497e5f29e551d95be8a3d16a655ec71e8549
moncho <ramon.crespo@ventura24.es>
2018-06-12 17:11:52 +0200

    Add useless app source code

---
+++ src/main/java/useless/Controller.java
@@ -0,0 +1,14 @@
+package useless;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Leave me alone!";
+    }
+
+}
